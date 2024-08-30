c0756301625cd1117ea314f0641e2a2bb2c2d6cd
ristukaJJang <ghwjd5585@gmail.com>
2024-08-30 11:43:36 +0900

    08-30 11:43

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import { Outlet, Route, Routes, useLocation } from 'react-router';
 import QueryString from './router/QueryString';
 import PathVariable from './router/PathVariable';
 import PathMove from './router/PathMove';
+import Zustand from './zustand/Zustand';
 
 // react-router 패키지 : react의 SPA(Single Page Application)에서 라우팅을 구현하기 위한 라이브러리
 // - 웹 페이지에서 다양한 경로에 따른 뷰를 관리할 수 있도록 함
@@ -70,7 +71,7 @@ function App() {
         <Route path='path-variable/:name' element={<PathVariable />}/>
         <Route path='path-move' element={<PathMove />}/>
       </Route>
-
+      <Route path='/zustand' element={<Zustand />}/>
       <Route path='*' element={<h1>404</h1>}/>
       
       {/* <Properties /> 
